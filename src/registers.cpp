00009d6527a8361a3264badd60d001821a4e12de
Ryan Blecher <notryanb@gmail.com>
2025-08-21 22:18:19 -0400

    continuing chapter 5. Almost done, need to figure out why tests are failing

--- src/registers.cpp
+++ src/registers.cpp
@@ -1,5 +1,7 @@
 #include <libsdb/registers.hpp>
 #include <libsdb/bit.hpp>
+#include <libsdb/process.hpp>
+#include <iostream>
 
 sdb::registers::value sdb::registers::read(const register_info& info) const {
   auto bytes = as_bytes(data_);
@@ -24,5 +26,29 @@ sdb::registers::value sdb::registers::read(const register_info& info) const {
   }
   else {
     return from_bytes<byte128>(bytes + info.offset);
+  }
+}
+
+void sdb::registers::write(const register_info& info, value val) {
+  auto bytes = as_bytes(data_);
+
+  std::visit([&](auto& v) {
+    if (sizeof(v) == info.size) {
+      auto val_bytes = as_bytes(v);
+      std::copy(val_bytes, val_bytes + sizeof(v), bytes + info.offset);
+    } else {
+      std::cerr << "sdb::register::write call with mismatched register and value sizes";
+      std::terminate();
+    }
+               
+  }, val);
+
+  if (info.type == register_type::fpr) {
+    proc_->write_fprs(data_.i387);    
+  } else {
+    // 8 bit registers are offset by a byte into the super-register.
+    // set the lowest 3 bits to 0, which forces 8-byte alignment
+    auto aligned_offset = info.offset & ~0b111;
+    proc_->write_user_area(info.offset, from_bytes<std::uint64_t>(bytes + info.offset));
   }
 }
