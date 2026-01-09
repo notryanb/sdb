340baeae33b65c1757322ca180385bedf85a2b34
Ryan Blecher <notryanb@gmail.com>
2026-01-08 20:56:47 -0500

    Chapter 7 - Add breakpoint commands and help documentation to sdb tool

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -40,6 +40,24 @@ void sdb::breakpoint_site::enable() {
   is_enabled_ = true;
 }
 
+void sdb::breakpoint_site::disable() {
+  if (!is_enabled_) return;
+
+  errno = 0;
+  std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), address_, nullptr);
+  if (errno != 0) {
+    error::send_errno("Disabling breakpoint site failed");
+  }
+
+  auto restored_data = ((data & ~0xff) | static_cast<std::uint8_t>(saved_data_));
+
+  if (ptrace(PTRACE_POKEDATA, process_->pid(), address_, restored_data) < 0) {
+    error::send_errno("Disabling breakpoint site failed");
+  }
+
+  is_enabled_ = false;
+}
+
 sdb::breakpoint_site::breakpoint_site(process& proc, virt_addr address)
   : process_{ &proc }, address_{ address }, is_enabled_{ false },saved_data_{} {
     id_ = get_next_id();
