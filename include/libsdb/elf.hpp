cf0c89b207394037af1b16270baeb63f9c33fb73
Ryan Blecher <notryanb@gmail.com>
2026-04-12 15:45:52 -0400

    Chapter 11 - Start work on parsing sections and the string tables

--- include/libsdb/elf.hpp
+++ include/libsdb/elf.hpp
@@ -3,6 +3,12 @@
 
 #include <filesystem>
 #include <elf.h>
+#include <optional>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+#include <libsdb/types.hpp>
+
 
 namespace sdb {
   class elf {
@@ -16,12 +22,22 @@ namespace sdb {
       std::filesystem::path path() const { return path_; }
       const Elf64_Ehdr& get_header() const { return header_; }
 
+      std::string_view get_section_name(std::size_t index) const;
+
+      std::optional<const Elf64_Shdr*> get_section(std::string_view name) const;
+      span<const std::byte> get_section_contents(std::string_view name) const;
+
     private:
+      void build_section_map();
+      void parse_section_headers();
+      
       int fd_;
       std::filesystem::path path_;
       std::size_t file_size_;
       std::byte* data_;
       Elf64_Ehdr header_;
+      std::vector<Elf64_Shdr> section_headers_;
+      std::unordered_map<std::string_view, Elf64_Shdr*> section_map_;
   };
 }
 
--- src/elf.cpp
+++ src/elf.cpp
@@ -32,9 +32,57 @@ sdb::elf::elf(const std::filesystem::path& path) {
   data_ = reinterpret_cast<std::byte*>(ret);
 
   std::copy(data_, data_ + sizeof(header_), as_bytes(header_));
+
+  parse_section_headers();
+  build_section_map();
 }
 
 sdb::elf::~elf() {
   munmap(data_, file_size_);
   close(fd_);
+}
+
+std::string_view sdb::elf::get_section_name(std::size_t index) const {
+  auto& section = section_headers_[header_.e_shstrndx];
+  return { reinterpret_cast<char*>(data_) + section.sh_offset + index };
+}
+
+std::optional<const Elf64_Shdr*> sdb::elf::get_section(std::string_view name) const {
+  if (section_map_.count(name) == 0) {
+    return std::nullopt;
+  }
+
+  return section_map_.at(name);
+}
+
+sdb::span<const std::byte> sdb::elf::get_section_contents(std::string_view name) const {
+  if (auto sect = get_section(name); sect) {
+    return { data_ + sect.value()->sh_offset, sect.value()->sh_size };
+  }
+
+  return { nullptr, std::size_t(0) };
+}
+
+/* private methods  */
+void sdb::elf::build_section_map() {
+  for (auto& section : section_headers_) {
+    section_map_[get_section_name(section.sh_name)] = &section;
+  }  
+}
+
+void sdb::elf::parse_section_headers() {
+  auto n_headers = header_.e_shnum;
+
+  if (n_headers == 0 and header_.e_shentsize != 0) {
+    n_headers = from_bytes<Elf64_Shdr>(data_ + header_.e_shoff).sh_size;
+  }
+
+  
+  section_headers_.resize(n_headers);
+
+  std::copy(
+    data_ + header_.e_shoff,
+    data_ + header_.e_shoff + sizeof(Elf64_Shdr) * n_headers,
+    reinterpret_cast<std::byte*>(section_headers_.data())
+  );
 }
